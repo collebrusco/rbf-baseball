f08935bf3090a37f472785b993ef987c914bcb67
collebrusco <collebrusco.frank@gmail.com>
2023-12-27 22:31:22 -0600

    amaze. perfect. amaze. yes.

--- src/main.cpp
+++ src/main.cpp
@@ -41,9 +41,10 @@ void createCylinder(std::vector<glm::vec3>& vertices, std::vector<glm::vec3>& no
 
     // Clear vectors
     vertices.clear();
+    normals.clear();
     indices.clear();
 
-    // Generate vertices
+    // Generate vertices for side surface
     for (int i = 0; i < 2; ++i) {
         float y = i * height - halfHeight;
         for (int j = 0; j < numCircleVertices; ++j) {
@@ -51,8 +52,19 @@ void createCylinder(std::vector<glm::vec3>& vertices, std::vector<glm::vec3>& no
             float x = radius * cos(angle);
             float z = radius * sin(angle);
             vertices.push_back(glm::vec3(x, y, z));
+
+            // Compute normal for side (without considering y-component)
+            glm::vec3 normal = glm::normalize(glm::vec3(x, 0.0f, z));
+            normals.push_back(normal);
         }
     }
+
+    // Add center vertices for the top and bottom faces
+    vertices.push_back(glm::vec3(0.0f, halfHeight, 0.0f)); // Top center
+    normals.push_back(glm::vec3(0.0f, 1.0f, 0.0f));       // Normal pointing upwards
+
+    vertices.push_back(glm::vec3(0.0f, -halfHeight, 0.0f)); // Bottom center
+    normals.push_back(glm::vec3(0.0f, -1.0f, 0.0f));        // Normal pointing downwards
 
     // Generate indices for the side surface
     for (int i = 0; i < numCircleVertices; ++i) {
@@ -68,23 +80,25 @@ void createCylinder(std::vector<glm::vec3>& vertices, std::vector<glm::vec3>& no
         indices.push_back(next);
     }
 
+    // Generate indices for the top and bottom faces
+    int topCenterIndex = 2 * numCircleVertices;
+    int bottomCenterIndex = topCenterIndex + 1;
 
-    // Generate vertices and normals
-    for (int i = 0; i < 2; ++i) {
-        float y = i * height - halfHeight;
-        for (int j = 0; j < numCircleVertices; ++j) {
-            float angle = 2.0f * M_PI * j / numCircleVertices;
-            float x = radius * cos(angle);
-            float z = radius * sin(angle);
-            vertices.push_back(glm::vec3(x, y, z));
+    for (int i = 0; i < numCircleVertices; ++i) {
+        int next = (i + 1) % numCircleVertices;
 
-            // Compute normal (without considering y-component)
-            glm::vec3 normal = glm::normalize(glm::vec3(x, 0.0f, z));
-            normals.push_back(normal);
-        }
+        // Top face
+        indices.push_back(bottomCenterIndex);
+        indices.push_back(i);
+        indices.push_back(next);
+
+        // Bottom face
+        indices.push_back(topCenterIndex);
+        indices.push_back(i + numCircleVertices);
+        indices.push_back(next + numCircleVertices);
     }
-
 }
+
 
 struct rbfVt {
 	vec3 pos, normal;
@@ -112,6 +126,7 @@ static void setup_cyl() {
 	vector<uint32_t> ivec;
 	createCylinder(vvec, nvec, ivec, 16, 2);
 	vector<rbfVt> rbfVec;
+	std::cout << "indices vector size: " << ivec.size() << std::endl;
 	for (uint32_t i = 0; i < vvec.size(); i++) {
 		rbfVec.push_back({{vvec[i]}, {nvec[i]}});
 	}
@@ -137,11 +152,15 @@ static Texture grass;
 
 static mat4 cylmat = genModelMat3d(vec3(0., 0., 10.), vec3(0.), vec3(0.5, 2, 0.5));
 
-static int base_index = 0;
-static mat4 cylmat_first_base = genModelMat3d(vec3(1., -1., 10.), vec3(0.), vec3(0.25, 0.05, 0.25));
-static mat4 cylmat_second_base = genModelMat3d(vec3(1., -1., 10.), vec3(0.), vec3(0.25, 0.05, 0.25));
-static mat4 cylmat_third_base = genModelMat3d(vec3(1., -1., 10.), vec3(0.), vec3(0.25, 0.05, 0.25));
-static mat4 cylmat_home_base = genModelMat3d(vec3(1., -1., 10.), vec3(0.), vec3(0.25, 0.05, 0.25));
+static mat4 cylmat_first_base = genModelMat3d(vec3(-2., -1., 6.), vec3(0.), vec3(0.25, 0.05, 0.25));
+static mat4 cylmat_second_base = genModelMat3d(vec3(0., -1., 8.), vec3(0.), vec3(0.25, 0.05, 0.25));
+static mat4 cylmat_third_base = genModelMat3d(vec3(2, -1., 6.), vec3(0.), vec3(0.25, 0.05, 0.25));
+static mat4 cylmat_home_base = genModelMat3d(vec3(0., -1., 2), vec3(0.), vec3(0.25, 0.05, 0.25));
+
+
+static mat4 pitcher = genModelMat3d(vec3(0., -0.5, 6), vec3(0.), vec3(0.05, 0.5, 0.05));
+
+static mat4 ball = genModelMat3d(vec3(0.08, -0.5, 6), vec3(0.), vec3(0.05, 0.05, 0.05));
 
 int main() {
 
@@ -149,6 +168,7 @@ int main() {
 	flgl::config.set_shader_path("shad/");
 	flgl::config.set_asset_path("assets/");
 	flgl::window.create("rbf baseball", 1280, 1280);
+	flgl::gl.set_clear_color(0.3, 0.6, 0.9, 1);
 	flgl::gl.enable_depth_test(true);
 	grass = Texture::from_file("grass", true);
 
@@ -161,9 +181,12 @@ int main() {
 	vec3 cameraPos = vec3(0., 0., 0.);
 	float cameraVelocity = 0.1;
 
+	float ballX = 0.08, ballY = -0.5, ballZ = 6;
+
 	// cam.setMouseControl(true);
 	// window.grab_mouse();
-	float x = 0, y = 0, z = 0;
+
+	float ballVelocity = 0.01;
 
 	while (!window.should_close()) { 
 		flgl::gl.clear();
@@ -172,44 +195,41 @@ int main() {
 			flgl::gl.wireframe(wf); wf = !wf;
 		}
 		if (window.keyboard[GLFW_KEY_W].down) {
-			// cameraPos.z += cameraVelocity;
-			z += 0.05;
+			cameraPos.z += cameraVelocity;
 		}
 		if (window.keyboard[GLFW_KEY_S].down) {
-			// cameraPos.z -= cameraVelocity;
-			z -= 0.05;
+			cameraPos.z -= cameraVelocity;
 		}
 		if (window.keyboard[GLFW_KEY_A].down) {
-			// cameraPos.x += cameraVelocity;
-			x += 0.05;
+			cameraPos.x += cameraVelocity;
 		}
 		if (window.keyboard[GLFW_KEY_D].down) {
-			// cameraPos.x -= cameraVelocity;
-			x -= 0.05;
+			cameraPos.x -= cameraVelocity;
 		}
 		if (window.keyboard[GLFW_KEY_Q].down) {
-			// cameraPos.y += cameraVelocity;
-			y += 0.05;
+			cameraPos.y += cameraVelocity;
 		}
 		if (window.keyboard[GLFW_KEY_E].down) {
-			// cameraPos.y -= cameraVelocity;
-			y -= 0.05;
+			cameraPos.y -= cameraVelocity;
 		}
-		if (window.keyboard[GLFW_KEY_B].pressed) {
-			std::cout << base_index << ": " << x << ", " << y << ", " << z << std::endl;
-			base_index = (base_index + 1) % 4;
+
+		if (window.keyboard[GLFW_KEY_SPACE].pressed) {
+			static int score = 0;
+			if (ballZ >= 1.5 && ballZ <= 2.5) {
+				std::cout << "HIT! score: " << ++score << std::endl;
+				ballZ = 6;
+				ballVelocity *= 2;
+			} else {
+				std::cout << "STRIKE" << std::endl;
+				*((char*)0) = 1;
+			}
 		}
-		if (base_index == 0) {
-			cylmat_first_base = genModelMat3d(vec3(x, y, z), vec3(0.), vec3(0.25, 0.05, 0.25));
-		} else if(base_index == 1) {
-			cylmat_second_base = genModelMat3d(vec3(x, y, z), vec3(0.), vec3(0.25, 0.05, 0.25));
-		} else if(base_index == 2) {
-			cylmat_third_base = genModelMat3d(vec3(x, y, z), vec3(0.), vec3(0.25, 0.05, 0.25));
-		} else if(base_index == 3) {
-			cylmat_home_base = genModelMat3d(vec3(x, y, z), vec3(0.), vec3(0.25, 0.05, 0.25));
-		}
+		ballZ -= ballVelocity;
+		
 		cam.getPos() = cameraPos;
 		cam.update();
+
+		ball = genModelMat3d(vec3(ballX, ballY, ballZ), vec3(0.), vec3(0.05, 0.05, 0.05));
 
 
 		quad.bind();
@@ -234,20 +254,31 @@ int main() {
 
 		cyl_shader.uMat4("uModel", cylmat_first_base);
 		cyl.bind();
-		flgl::gl.draw_elements(GL_TRIANGLES, 16*6, GL_UNSIGNED_INT);
+		flgl::gl.draw_elements(GL_TRIANGLES, 16*6+96, GL_UNSIGNED_INT);
 		cyl_shader.uMat4("uModel", cylmat_second_base);
 		cyl.bind();
-		flgl::gl.draw_elements(GL_TRIANGLES, 16*6, GL_UNSIGNED_INT);
+		flgl::gl.draw_elements(GL_TRIANGLES, 16*6+96, GL_UNSIGNED_INT);
 		cyl_shader.uMat4("uModel", cylmat_third_base);
 		cyl.bind();
-		flgl::gl.draw_elements(GL_TRIANGLES, 16*6, GL_UNSIGNED_INT);
+		flgl::gl.draw_elements(GL_TRIANGLES, 16*6+96, GL_UNSIGNED_INT);
 		cyl_shader.uMat4("uModel", cylmat_home_base);
 		cyl.bind();
-		flgl::gl.draw_elements(GL_TRIANGLES, 16*6, GL_UNSIGNED_INT);
+		flgl::gl.draw_elements(GL_TRIANGLES, 16*6+96, GL_UNSIGNED_INT);
+		cyl_shader.uMat4("uModel", ball);
+		cyl.bind();
+		flgl::gl.draw_elements(GL_TRIANGLES, 16*6+96, GL_UNSIGNED_INT);
+		cyl_shader.uMat4("uModel", pitcher);
+		cyl.bind();
+		flgl::gl.draw_elements(GL_TRIANGLES, 16*6+96, GL_UNSIGNED_INT);
 
 		window.update();
 	}
 
+	grass_shader.destroy();
+	cyl_shader.destroy();
+	cyl.destroy();
+	quad.destroy();
+	flgl::window.destroy();
 	flgl::gl.destroy();
 
 
